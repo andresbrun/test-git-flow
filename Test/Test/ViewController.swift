e7190c4dcf030b8998826f7968c5812309c7dc67
andresbrun <andres.brun@cabify.com>
2018-08-27 15:59:33 +0200

    Example project added

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Andrés Brun on 27/08/2018.
+//  Copyright © 2018 Cabify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
